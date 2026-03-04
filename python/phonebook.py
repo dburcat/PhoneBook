2507fe33d00e4c1a0507695a1fa10a76127dc129
david <dburcad@gmail.com>
2026-03-04 14:16:08 -0500

    Finished Phonebook

--- python/phonebook.py
+++ python/phonebook.py
@@ -13,7 +13,7 @@ class PhoneBook:
         Constructor for PhoneBook
         :param phonebook_dict: Optional dictionary to initialize the phonebook with
         """
-        self.phonebook = None
+        self.phonebook = {} if phonebook_dict is None else phonebook_dict
 
     def add(self, name: str, phone_number: str) -> None:
         """
@@ -21,7 +21,11 @@ class PhoneBook:
         :param name: Contact name
         :param phone_number: Phone number to add
         """
-        pass
+        if name not in self.phonebook:
+            self.phonebook[name] = []
+        if phone_number not in self.phonebook[name]:
+            print(self.phonebook[name])
+            self.phonebook[name].append(phone_number)
 
     def add_all(self, name: str, *phone_numbers: str) -> None:
         """
@@ -29,14 +33,16 @@ class PhoneBook:
         :param name: Contact name
         :param phone_numbers: Variable number of phone numbers to add
         """
-        pass
+        for phone_number in phone_numbers:
+            self.add(name, phone_number)
 
     def remove(self, name: str) -> None:
         """
         Remove a contact from the phonebook
         :param name: Contact name to remove
         """
-        pass
+        if name in self.phonebook:
+            del self.phonebook[name]
 
     def has_entry(self, name: str, phone_number: str = None) -> bool:
         """
@@ -45,7 +51,10 @@ class PhoneBook:
         :param phone_number: Optional phone number to check
         :return: True if contact exists (with phone number if specified), False otherwise
         """
-        return None
+        if phone_number is None:
+            return name in self.phonebook
+        else:
+            return name in self.phonebook and phone_number in self.phonebook[name]
 
     def lookup(self, name: str) -> List[str]:
         """
@@ -53,7 +62,7 @@ class PhoneBook:
         :param name: Contact name to look up
         :return: List of phone numbers for the contact
         """
-        return None
+        return self.phonebook.get(name, [])
 
     def reverse_lookup(self, phone_number: str) -> str:
         """
@@ -61,18 +70,42 @@ class PhoneBook:
         :param phone_number: Phone number to look up
         :return: Contact name associated with the phone number
         """
-        return None
+        return next((name for name, numbers in self.phonebook.items() if phone_number in numbers), None)
 
     def get_all_contact_names(self) -> List[str]:
         """
         Get all contact names in the phonebook
         :return: List of all contact names
         """
-        return None
+        return list(self.phonebook.keys())
 
     def get_map(self) -> Dict[str, List[str]]:
         """
         Get the underlying dictionary representation of the phonebook
         :return: Dictionary mapping names to lists of phone numbers
         """
+        return self.phonebook
+    
+    def __str__(self) -> str:
+        """
+        String representation of the phonebook
+        :return: String representation of the phonebook
+        """
         return None
+    
+def main():
+    phone_book = PhoneBook()
+    # phone_book.add_all("Joe", "302-555-4444", "302-555-3333", "302-555-2222", "302-555-1111")
+    # phone_book.add("John", "302-555-4545")
+    # phone_book.add_all("Smith", "302-554-4535", "302-554-4536")
+    # phone_book.add("Smith", "302-554-4536")
+    # print(phone_book.lookup("Joe"))
+    # print(phone_book.has_entry("Joe", "302-555-3333"))
+    # print(phone_book.has_entry("Joe", "302-555-9999"))
+    # print(phone_book.reverse_lookup("302-555-3333"))
+    # print(phone_book.get_all_contact_names())
+    # print(phone_book.get_map())
+    # print(phone_book.reverse_lookup("302-555-9999"))  # Should return None
+
+if __name__ == "__main__":
+    main()
